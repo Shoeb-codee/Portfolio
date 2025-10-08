195dad41004224b854651194094cc8630e622d23
Shoeb-codee <shoebahmad698@gmail.com>
2025-10-08 13:30:40 +0530

    update Navbar styles for hover effects and improve consistency

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -50,21 +50,21 @@ const Navbar = () => {
       <ul  id='nav-icons' className={`  ${menu?"block":"hidden"}    mx-24 py-2 mt-4 font-semibold md:mt-5 bg-black px-2 rounded-xl bg-opacity-30 md:border-none text-center md:bg-transparent md:static md:mx-0 md:flex gap-6`} onClick={()=>{ if(!showMenu){ openMenu(false); setshowMenu(true);} }}>
       
 <a href="#Home">
-        <li className='text-md md:text-lg transition-all duration-300 p-1 md:p-0 md:hover:text-red-600 md:hover:text-xl' > Home</li>
+        <li className='text-md md:text-lg transition-all duration-300 p-1 md:p-0 md:hover:text-red-500 md:hover:text-xl' > Home</li>
 
 </a>
 <a href="#About">
-        <li  className='text-md md:text-lg transition-all duration-300 p-1 md:p-0 md:hover:text-red-600 md:hover:text-xl'>About</li>
+        <li  className='text-md md:text-lg transition-all duration-300 p-1 md:p-0 md:hover:text-red-500 md:hover:text-xl'>About</li>
 
 </a>
 <a href="#Skills">
-        <li  className='text-md md:text-lg transition-all duration-300 p-1 md:p-0 md:hover:text-red-600 md:hover:text-xl '>Skills</li>
+        <li  className='text-md md:text-lg transition-all duration-300 p-1 md:p-0 md:hover:text-red-500 md:hover:text-xl '>Skills</li>
   </a>        
 <a href="#Projects">
-        <li  className='text-md md:text-lg transition-all duration-300 p-1 md:p-0 md:hover:text-red-600 md:hover:text-xl'>Projects</li>
+        <li  className='text-md md:text-xl text-cyan-500 transition-all duration-300 p-1 md:p-0 md:hover:text-emerald-500 md:hover:text-2xl'>Projects</li>
   </a>       
 <a href="#Contacts">
-        <li className='text-md md:text-lg transition-all duration-300 p-1 md:p-0 md:hover:text-red-600 md:hover:text-xl '>Contacts</li>
+        <li className='text-md md:text-lg transition-all duration-300 p-1 md:p-0 md:hover:text-red-500 md:hover:text-xl '>Contacts</li>
   </a>     
    </ul>
       {
